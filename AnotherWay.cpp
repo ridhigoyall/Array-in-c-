8e25f031d9e51d959679d13fc0ec6b57385a0aa3
ridhigoyall <ridhigoyal00510@gmail.com>
2023-07-21 22:07:51 +0530

    first commit

---
+++ AnotherWay.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int myArray[3];
+    myArray[0]=30;
+    myArray[1]=42;
+    myArray[2]=50;
+    cout<<myArray[0]<<endl;
+    cout<<myArray[1]<<endl;
+    cout<<myArray[2]<<endl;
+    return 0;
+}
---
+++ chapter-2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int myArray[15];
+    for (int i=0;i<=14;i++)
+    {
+
+        myArray[i]=67;
+        cout<<i<<"="<<myArray[i]<<endl;
+    }
+    return 0;
+}
